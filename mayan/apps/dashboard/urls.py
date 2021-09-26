f51f3f7373b83a8e2e2d6f1a56abf58072fc4f32
Teddy Liang <teddytduoliang@gmail.com>
2021-09-26 16:29:17 -0400

    added the Candidates dashboard

--- mayan/apps/dashboard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from .views import ( DashboardListView )
-
-urlpatterns = [
-    url(
-        regex=r'^dashboard/$', name='dashboard',
-        view=DashboardListView.as_view()
-        )
-]
