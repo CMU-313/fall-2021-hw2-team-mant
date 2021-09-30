0c6d26cc0b7d3f90f15202e275820c24238c12ad
Anagha Srikumar <asrikuma@andrew.cmu.edu>
2021-09-29 23:10:33 -0400

    trying to fix conflicts on dashboard merge

--- mayan/apps/dashboard/permissions.py
+++
@@ -1,9 +0,0 @@
-from django.utils.translation import ugettext_lazy as _
-
-from mayan.apps.permissions import PermissionNamespace
-
-namespace = PermissionNamespace(label=_('Dashboard'), name='Dashboard')
-
-permission_dashboard_view = namespace.add_permission(
-    label=_('View'), name='dashboard_view'
-)
