d0cd944760f0363b8412cce2c8f312e14c9af9e2
Teddy Liang <teddytduoliang@gmail.com>
2021-09-30 01:02:46 -0400

    testing merging issues

--- mayan/apps/dashboard/views.py
+++ mayan/apps/dashboard/views.py
@@ -4,5 +4,7 @@ from django.views import View
 class DashboardListView(View):
 
     def get(self, request, *args, **kwargs):
-        html = "<html><body>THIS IS MY HTML CODE</body></html>"
+        dashboard_html = open("dashboard_view.html", 'r')
+        html = dashboard_html.read() 
+        # html = "<html><body>THIS IS MY HTML CODE testing testing testing</body></html>"
         return HttpResponse(html)
